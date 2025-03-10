4e1890ccc627f98fce5a5187c737d5c6541c15a5
simonnnn777777 <simonfischer184@icloud.com>
2025-03-10 19:11:24 +0100

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,16 @@
 fx_version 'cerulean'
 game 'gta5'
 
+lua54 'yes'
+
 author 'SM X Simon'
-description 'Admin Fuel'
-version '1.0.0'
+description 'Admin Fuel Command'
+version '1.2.1'
 
 server_script 'server.lua'
 client_script 'client.lua'
+
+shared_script '@ox_lib/init.lua' -- WICHTIG für ox_lib
+dependencies {
+    'ox_lib' -- ox_lib als Abhängigkeit setzen
+}
